8543fc65cddeb87b152265716e9cee38fc179f12
fabo <fabo.dev.email@gmail.com>
2023-11-28 18:06:18 +0100

    #4
    init items resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-
 import { ConfigModule } from '@nestjs/config';
+
 import { DatabaseModule } from './database/database.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
