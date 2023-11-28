33ffa2cc7f4ca87253645a02f86cfbb291d0ecdf
fabo <fabo.dev.email@gmail.com>
2023-11-28 23:12:38 +0100

    #5
    item entity

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,18 @@
-export class Item {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ default: true })
+  public: boolean;
+
+  constructor(item: Partial<Item>) {
+    // auto assign the passed in properties the new instance
+    Object.assign(this, item);
+  }
+}
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Item])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
