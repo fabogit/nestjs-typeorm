00cff0666a82ba1bd5f00e506fdf30cde7b57ae8
fabo <fabo.dev.email@gmail.com>
2023-11-28 18:01:32 +0100

    #3
    config and db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [],
   providers: [],
 })
